2fd127f07c02ab05770890ae0f460f5e810cdd47
Anuraag Agrawal <anuraaga@gmail.com>
2021-10-06 12:42:31 +0900

    Add release workflows (#2)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
     id("com.google.protobuf") version "0.8.17"
     id("de.undercouch.download") version "4.1.2"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("nebula.release") version "16.0.0"
   }
 }
 
