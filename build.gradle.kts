a22fa1a594db9a5d527a2cbb6f2bc5465520679b
Anuraag Agrawal <anuraaga@gmail.com>
2022-02-11 11:33:13 +0900

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,8 @@ tasks {
 
 description = "Java Bindings for the OpenTelemetry Protocol (OTLP)"
 
-val grpcVersion = "1.41.0"
-val protobufVersion = "3.18.0"
+val grpcVersion = "1.44.0"
+val protobufVersion = "3.19.4"
 
 repositories {
   mavenCentral()
