7eed9d340d422023e8465ae03117dd867789e12c
jason plumb <75337021+breedx-splk@users.noreply.github.com>
2024-07-24 13:53:21 -0700

    Update dependencies (#16)

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,8 @@ tasks {
 
 description = "Java Bindings for the OpenTelemetry Protocol (OTLP)"
 
-val grpcVersion = "1.56.1"
-val protobufVersion = "3.23.4"
+val grpcVersion = "1.65.1"
+val protobufVersion = "4.27.2"
 
 repositories {
   mavenCentral()
