ba7c1427700346b7294a84755843e173094418d6
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-10-21 21:20:42 -0700

    Address gradle toolchain deprecation warning (#142)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,12 @@ pluginManagement {
     id("com.google.protobuf") version "0.9.5"
     id("de.undercouch.download") version "5.6.0"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
   }
+}
+
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention")
 }
 
 rootProject.name = "opentelemetry-proto"
